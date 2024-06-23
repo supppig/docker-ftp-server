404decda5d9985c93aca15f10922cfaeef56e57a
supppig <supppig@163.com>
2024-06-23 10:56:54 +0800

    Update docker-entrypoint.sh

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -14,7 +14,7 @@ adduser \
 	$FTP_USER
 
 mkdir -p /home/$FTP_USER
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+#chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
 
 touch /var/log/vsftpd.log
