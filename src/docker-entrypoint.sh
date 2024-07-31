7f5e88c40ff77efbbf8120fb7b8427fc0e05773f
supppig <supppig@163.com>
2024-07-31 15:42:37 +0000

    update

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-replaceFile() {
-	if [ -f "/etc/$1" ];then
-		if [ -z "$(ls -F "/etc/$1" | grep '@')" ];then
-			rm -f /etc/$1
-			ln -s /pure-ftpd/$1 /etc/$1
-		fi
-	else
-		ln -s /pure-ftpd/$1 /etc/$1
-	fi	
-}
-
-
 if [ -d "/pure-ftpd" ];then
-	for x in $(ls /pure-ftpd); do
-		replaceFile $x
-	done
+	/pure-config.pl /pure-ftpd/pure-ftpd.conf
 fi
-
-/usr/sbin/pure-ftpd
+tail -f /dev/null
